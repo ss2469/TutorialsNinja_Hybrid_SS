f1fba94a8e0debd923eb712a34e8a9115c887341
ss2469 <sumita2469@gmail.com>
2024-08-14 00:07:07 -0400

    completed checkout testcases

--- src/test/java/com/tutorialsninja/pages/CheckoutPage.java
+++ src/test/java/com/tutorialsninja/pages/CheckoutPage.java
@@ -1,16 +1,200 @@
 package com.tutorialsninja.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
-public class CheckoutPage {
-	
+import com.tutorialsninja.testbase.TestBase;
+
+public class CheckoutPage extends TestBase {
+
+	public CheckoutPage(WebDriver driver) throws Exception {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
 	public WebDriver driver;
-	/*
-	@FindBy()
-	private WebElement 
-	
-	
-	*/
+	public HomePage homepage;
+	public ProductPage productpage;
+	public AddToCartPage addtocartpage;
+	public CheckoutPage checkoutPage;
+
+	@FindBy(xpath = "//div/a[ text() = 'Checkout']")
+	private WebElement checkoutButton;
+
+	@FindBy(xpath = "//a[@title = 'Shopping Cart']")
+	private WebElement shoppingcartLink;
+
+	@FindBy(xpath = "//input[@name = 'payment_address' and @value = 'existing']")
+	private WebElement radioButtonBilling;
+
+	@FindBy(css = "#button-payment-address")
+	private WebElement continueButtonBillingAddress;
+
+	@FindBy(xpath = "//input[@name = 'shipping_address' and @value = 'existing']")
+	private WebElement radioButtonDelivery;
+
+	@FindBy(css = "#button-shipping-address")
+	private WebElement continueButtonDeliveryAddress;
+
+	@FindBy(xpath = "//input[@name = 'shipping_method']")
+	private WebElement radioButtonFlatRate;
+
+	@FindBy(css = "#button-shipping-method")
+	private WebElement continueButtonDeliveryMethod;
+
+	@FindBy(xpath = "//input[@name = 'payment_method' and @value = 'cod']")
+	private WebElement cashOnDeliveryRadioButton;
+
+	@FindBy(css = "input[name = 'agree']")
+	private WebElement termsAndConditionsCheckbox;
+
+	@FindBy(css = "#button-payment-method")
+	private WebElement continueButtonPaymentMethod;
+
+	@FindBy(css = "div>#button-confirm")
+	private WebElement confirmOrderButton;
+
+	@FindBy(xpath = "//p[text() = 'Your order has been successfully processed!']")
+	private WebElement successMessageOfOrderPlacement;
+
+	@FindBy(xpath = "//input[@value = 'guest']")
+	private WebElement guestAccountRadioButton;
+
+	@FindBy(css = "#button-account")
+	private WebElement continueButtonOfNewCustomer;
+
+	@FindBy(css = "#input-payment-firstname")
+	private WebElement firstnameTextBox;
+
+	@FindBy(css = "#input-payment-lastname")
+	private WebElement lastnameTextBox;
+
+	@FindBy(css = ".form-group.required>input[name = 'email']")
+	private WebElement emailTextBox;
+
+	@FindBy(css = ".form-group.required>input[name = 'telephone']")
+	private WebElement telephoneTextBox;
+
+	@FindBy(css = ".form-group.required>input[name = 'address_1']")
+	private WebElement address1TextBox;
+
+	@FindBy(css = ".form-group.required>input[name = 'city']")
+	private WebElement cityTextBox;
+
+	@FindBy(css = ".form-group.required>input[name = 'postcode']")
+	private WebElement postCodeTextBox;
+
+	@FindBy(css = "#button-guest")
+	private WebElement continueButtonGuest;
+
+	public void searchValidProductAndNavigateToCheckout() throws Exception {
+		homepage = new HomePage(driver);
+		checkoutPage = new CheckoutPage(driver);
+		productpage = homepage.navigateToProductPage(dataprop.getProperty("validProduct"));
+		productpage.verifyValidProductPresence();
+
+		addtocartpage = productpage.clickOnAddToCartButtonOnProductPage();
+		Assert.assertTrue(addtocartpage.displayStatusOfLaptopPrice());
+		addtocartpage.clickOnAddToCartButton();
+		Assert.assertTrue(addtocartpage.actualProductAddedSuccessMessage()
+				.contains(dataprop.getProperty("expectedProductAddedSuccessMessage")));
+		Assert.assertTrue(addtocartpage.verifyDisplayOfCartButtonWithItemsAndTotalPriceShowing());
+		addtocartpage.clickOnAddToCartButton();
+
+		checkoutPage.clickOnShoppingCartLink();
+		Assert.assertTrue(checkoutPage.verifyDisplayOfCheckoutButton());
+		checkoutPage.clickOnCheckoutButton();
+	}
+
+	public void clickOnCheckoutButton() {
+		checkoutButton.click();
+	}
+
+	public void clickOnShoppingCartLink() {
+		shoppingcartLink.click();
+	}
+
+	public boolean verifyDisplayOfCheckoutButton() {
+		boolean display = checkoutButton.isDisplayed();
+		return display;
+	}
+
+	public boolean verifySelectionOfRadioButtonBilling() {
+		boolean selected = radioButtonBilling.isSelected();
+		return selected;
+	}
+
+	public void clickOnContinueButtonOfBillingAddress() {
+		continueButtonBillingAddress.click();
+	}
+
+	public boolean verifySelectionOfRadioButtonDelivery() {
+		boolean selected = radioButtonDelivery.isSelected();
+		return selected;
+	}
+
+	public void clickOnContinueButtonOfDeliveryAddress() {
+		continueButtonDeliveryAddress.click();
+	}
+
+	public boolean verifySelectionOfRadioButtonDeliveryMethod() {
+		boolean selected = radioButtonFlatRate.isSelected();
+		return selected;
+	}
+
+	public void clickOnContinueButtonOfDeliveryMethod() {
+		continueButtonDeliveryMethod.click();
+	}
+
+	public boolean verifySelectionOfCashOnDeliveryRadioButton() {
+		boolean selected = cashOnDeliveryRadioButton.isSelected();
+		return selected;
+	}
+
+	public void clickOnTermsAndConditionsCheckbox() {
+		termsAndConditionsCheckbox.click();
+	}
+
+	public void clickOnContinueButtonOfPaymentMethod() {
+		continueButtonPaymentMethod.click();
+	}
+
+	public void clickOnConfirmOrderButton() {
+		confirmOrderButton.click();
+	}
+
+	public boolean verifyDisplayOfSuccessMessage() {
+		boolean display = successMessageOfOrderPlacement.isDisplayed();
+		return display;
+	}
+
+	public void clickOnGuestAccountRadioButton() {
+		guestAccountRadioButton.click();
+	}
+
+	public void clickOnContinueButtonOfNewCustomer() {
+		continueButtonOfNewCustomer.click();
+	}
+
+	public void fillingMandatoryDetailsOfGuestAccount(String firstnameText, String lastnameText, String emailText,
+			String telephoneText, String address1Text, String cityText, String postcodeText) {
+
+		firstnameTextBox.sendKeys(firstnameText);
+		lastnameTextBox.sendKeys(lastnameText);
+		emailTextBox.sendKeys(emailText);
+		telephoneTextBox.sendKeys(telephoneText);
+		address1TextBox.sendKeys(address1Text);
+		cityTextBox.sendKeys(cityText);
+		postCodeTextBox.sendKeys(postcodeText);
+
+	}
+
+	public void clickOnContinueButtonGuest() {
+		continueButtonGuest.click();
+	}
 
 }
--- src/test/java/com/tutorialsninja/testcases/CompleteCheckoutTest.java
+++ src/test/java/com/tutorialsninja/testcases/CompleteCheckoutTest.java
@@ -2,22 +2,17 @@ package com.tutorialsninja.testcases;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
-import com.tutorialsninja.pages.AddToCartPage;
-import com.tutorialsninja.pages.HomePage;
-import com.tutorialsninja.pages.LoginPage;
-import com.tutorialsninja.pages.ProductPage;
+import com.tutorialsninja.pages.CheckoutPage;
 import com.tutorialsninja.testbase.TestBase;
 import com.tutorialsninja.utilities.LoginUtil;
 
@@ -26,113 +21,76 @@ public class CompleteCheckoutTest extends TestBase {
 	public CompleteCheckoutTest() throws Exception {
 		super();
 	}
-	
+
 	public WebDriver driver;
-	public HomePage homepage;
-	public ProductPage productpage;
-	public AddToCartPage addtocartpage;
+	public CheckoutPage checkoutpage;
 	public LoginUtil loginutil;
-	public WebDriverWait wait; 
-	
-	
+	public WebDriverWait wait;
+
 	@BeforeMethod
 	public void completeCheckoutSetup() {
 		driver = initializeBrowserAndOpenApplication(configprop.getProperty("browser"));
 	}
 
-	
 	@Test(priority = 1)
 	public void validateCompleteCheckoutWithRegisteredAccount() throws Exception {
-		homepage = new HomePage(driver);
-		//search product and navigate to product page
-		productpage = homepage.navigateToProductPage(dataprop.getProperty("validProduct"));
-		productpage.verifyValidProductPresence();
-		//click on 'add to cart' button (on ProductPage)
-		addtocartpage = productpage.clickOnAddToCartButtonOnProductPage();
-		AssertJUnit.assertTrue(addtocartpage.displayStatusOfLaptopPrice());
-		addtocartpage.clickOnAddToCartButton();
-		AssertJUnit.assertTrue(addtocartpage.actualProductAddedSuccessMessage().contains(dataprop.getProperty("expectedProductAddedSuccessMessage")));
-		AssertJUnit.assertTrue(addtocartpage.verifyDisplayOfCartButtonWithItemsAndTotalPriceShowing());
-		addtocartpage.clickOnAddToCartButton();
-		
-		//Assert.assertTrue(driver.findElement(By.xpath("//p/a/strong/text()[contains(., 'View Cart')]")).isDisplayed());
-		//click on 'View Cart' link on DropDown of Cart Button
-		
-		driver.findElement(By.xpath("//a[@title = 'Shopping Cart']")).click();
-		AssertJUnit.assertTrue(driver.findElement(By.xpath("//div/a[text() = 'Checkout']")).isDisplayed());
-		driver.findElement(By.xpath("//div/a[text() = 'Checkout']")).click();
-		
+		checkoutpage = new CheckoutPage(driver);
+		checkoutpage.searchValidProductAndNavigateToCheckout();
+
 		loginutil = new LoginUtil(driver);
-		loginutil.loginWithValidCredentials(configprop.getProperty("validEmail"), configprop.getProperty("validPassword"));
-		
-		AssertJUnit.assertTrue(driver.findElement(By.xpath("//input[@name = 'payment_address' and @value = 'existing']")).isSelected());
-		driver.findElement(By.cssSelector("#button-payment-address")).click();
-		
-		AssertJUnit.assertTrue(driver.findElement(By.xpath("//input[@name = 'shipping_address' and @value = 'existing']")).isSelected());
-		driver.findElement(By.cssSelector("#button-shipping-address")).click();
-		
-		AssertJUnit.assertTrue(driver.findElement(By.xpath("//input[@name = 'shipping_method']")).isSelected());
-		driver.findElement(By.cssSelector("#button-shipping-method")).click();
+		loginutil.loginWithValidCredentials(configprop.getProperty("validEmail"),
+				configprop.getProperty("validPassword"));
 
-		AssertJUnit.assertTrue(driver.findElement(By.xpath("//input[@name = 'payment_method' and @value = 'cod']")).isSelected());
-		driver.findElement(By.cssSelector("input[name = 'agree']")).click();
-		
-		AssertJUnit.assertTrue(driver.findElement(By.cssSelector("div>#button-payment-method")).isDisplayed());
-		driver.findElement(By.cssSelector("div>#button-payment-method")).click();
-		
+		Assert.assertTrue(checkoutpage.verifySelectionOfRadioButtonBilling());
+		checkoutpage.clickOnContinueButtonOfBillingAddress();
+
+		Assert.assertTrue(checkoutpage.verifySelectionOfRadioButtonDelivery());
+		checkoutpage.clickOnContinueButtonOfDeliveryAddress();
+
+		Assert.assertTrue(checkoutpage.verifySelectionOfRadioButtonDeliveryMethod());
+		checkoutpage.clickOnContinueButtonOfDeliveryMethod();
+
+		Assert.assertTrue(checkoutpage.verifySelectionOfCashOnDeliveryRadioButton());
+		checkoutpage.clickOnTermsAndConditionsCheckbox();
+		checkoutpage.clickOnContinueButtonOfPaymentMethod();
+
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("div>#button-confirm")))).click();
-		
-		AssertJUnit.assertTrue(driver.findElement(By.xpath("//p[text() = 'Your order has been successfully processed!']")).isDisplayed());
-		
-	}
-	
-	
-	@Test(priority = 2, enabled = false)
-	public void validateCompleteCheckoutWithGuestAccount() {
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("div>#button-confirm"))));
+		checkoutpage.clickOnConfirmOrderButton();
 
-		
-								//Guest
-		//Checkout Options { Returning Customer }
-		/*enter email, password, click login button
-		 * 
-		 * OR //input[@value = 'guest']
-		 *    css - #button-account
-		 * 
-		 * click continue - Billing details
-		 * #button-payment-address
-		 * 
-		 * click continue - Delivery details
-		 * #button-shipping-address
-		 * 
-		 * click continue - delivery method
-		 * #button-shipping-method
-		 * 
-		 * click on terms & conditions checkbox
-		 * css - input[name = 'agree']
-		 * 
-		 * click on continue - payment method
-		 * #button-payment-method
-		 * 
-		 * click on button 'Confirm Order'
-		 * #button-confirm
-		 * 
-		 * 
-		 * Verify message - 'Your order has been successfully processed!'
-		 * //p[text() = 'Your order has been successfully processed!']
-		 */
-		//click continue button - //div/a[text() = 'Continue']
-		
-		
-		
+		Assert.assertTrue(checkoutpage.verifyDisplayOfSuccessMessage());
+
 	}
-	
+
+	@Test(priority = 2)
+	public void validateCompleteCheckoutWithGuestAccount() throws Exception {
+		checkoutpage = new CheckoutPage(driver);
+		checkoutpage.searchValidProductAndNavigateToCheckout();
+
+		checkoutpage.clickOnGuestAccountRadioButton();
+		checkoutpage.clickOnContinueButtonOfNewCustomer();
+
+		checkoutpage.fillingMandatoryDetailsOfGuestAccount(dataprop.getProperty("guestFirstName"),
+				dataprop.getProperty("guestLastName"), dataprop.getProperty("guestEmail"),
+				dataprop.getProperty("guestTelephone"), dataprop.getProperty("guestAddress1"),
+				dataprop.getProperty("guestCity"), dataprop.getProperty("guestPostCode"));
+
+		Select select = new Select(driver.findElement(By.id("input-payment-country")));
+		select.selectByVisibleText(dataprop.getProperty("guestCountry"));
+		select = new Select(driver.findElement(By.id("input-payment-zone")));
+		select.selectByVisibleText(dataprop.getProperty("guestState"));
+
+		checkoutpage.clickOnContinueButtonGuest();
+		checkoutpage.clickOnContinueButtonOfDeliveryMethod();
+		checkoutpage.clickOnTermsAndConditionsCheckbox();
+		checkoutpage.clickOnContinueButtonOfPaymentMethod();
+		checkoutpage.clickOnConfirmOrderButton();
+		Assert.assertTrue(checkoutpage.verifyDisplayOfSuccessMessage());
+	}
+
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
 	}
-	
-	
-	
 
 }
